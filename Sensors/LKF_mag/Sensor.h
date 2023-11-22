2a88eadd66433e9a1c35753e06dce92d33282814
Mikkel <71003572+Mikkelrw@users.noreply.github.com>
2023-11-22 15:41:34 +0100

    Update Sensor.h

--- Sensors/LKF_mag/Sensor.h
+++ Sensors/LKF_mag/Sensor.h
@@ -15,7 +15,8 @@ class Sensor{
 
   BLA::Matrix<3,1,float> accel_offset = {-0.0249,-0.0036,0.9617-1}; 
   BLA::Matrix<3,1,float> gyro_offset  = {-0.8630,-0.4062,-0.1584}; 
-  BLA::Matrix<3,1,float> magn_offset  = {-1,0,0};
+  BLA::Matrix<3,1,float> magn_soft_iron = {1,1,1};
+  BLA::Matrix<3,1,float> magn_hard_iron = {0,0,0};
 
   BLA::Matrix<3,1,float> accel_meas_filtered = {0,0,0};
   BLA::Matrix<3,1,float> gyro_meas_filtered = {0,0,0};
@@ -41,9 +42,9 @@ class Sensor{
   void Putty_Euler_Angles_Title();
   void Putty_Euler_Angles();
 
+  // Computes soft and hard iron effects
+  void Calibrate_magnetometer();
 
-  // Low-pass filter for measurements
-  void Filter_Measurements();
 
 
   // Prints to serial for Plotter to read directly. Requires line change as well
@@ -57,14 +58,21 @@ class Sensor{
   float Dot_Product(BLA::Matrix<3,1,float> vec1, BLA::Matrix<3,1,float> vec2);
     // Vector product form vec1 to vec2
   BLA::Matrix<3,1,float> vector_product(BLA::Matrix<3,1,float> vec1, BLA::Matrix<3,1,float> vec2);
+
+
   private:
 
   MPU9250_asukiaaa gatherer;
   Filter accel_filter[3];
 
-
-  // Updates the internal values with new measurements. Does as well remove the offset
+  // Updates the internal values with new raw measurements
   void Update_Measurements();
+  // Performs the offset removal of the measurements
+  void Offset_corrections();
+
+  // Low-pass filter for measurements (currently only accelerometer)
+  void Filter_Measurements();
+
   // Computes the euler angles using the normal methods.
   void Compute_Euler_Angles();
 
@@ -74,7 +82,8 @@ class Sensor{
   // Advances array of filter
   void Advance_Array(float*array,int length);
 
-
+  //Calibrate magnetometer
+  void get_Max_Min(float* samples, int size, float* max, float* min);
 
   // Normalizes vector
   BLA::Matrix<3,1,float> Normalize(BLA::Matrix<3,1,float>& vec_1);
@@ -87,4 +96,3 @@ class Sensor{
 
 
 #endif
-
