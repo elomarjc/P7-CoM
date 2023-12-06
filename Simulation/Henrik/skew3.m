a04b3adbcb19fbfc1c53c577ab7f9ea0781dd5ed
dsecad22 <dsecad22@student.aau.dk>
2023-12-06 12:31:07 +0100

    Deleting extra stuff
    Jusst junk of Simulink, no panics

--- Simulation/Henrik/skew3.m
+++
@@ -1,4 +0,0 @@
-function U = skew3(uvec)
-U = [0,-uvec(3),uvec(2);
-     uvec(3),0,-uvec(1);
-     -uvec(2),uvec(1),0];
--- Simulation/Henrik/skew4.m
+++
@@ -1,6 +0,0 @@
-function U = skew4(u_vec)
-U = [        u_vec(4), u_vec(3),-u_vec(2), u_vec(1);
-    -u_vec(3),        u_vec(4), u_vec(1), u_vec(2);
-     u_vec(2),-u_vec(1),        u_vec(4), u_vec(3);
-     -u_vec(1),-u_vec(2),-u_vec(3),        u_vec(4)];
-end
